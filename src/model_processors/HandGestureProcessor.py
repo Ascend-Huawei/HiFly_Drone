27cac3d47971f59e11e3998243efd5319efbedbc
jwillow19 <jeff.won@mail.utoronto.ca>
2021-08-05 15:50:35 +0000

    modified HandGesture postprocess output

--- src/model_processors/HandGestureProcessor.py
+++ src/model_processors/HandGestureProcessor.py
@@ -67,25 +67,23 @@ class ModelProcessor(BaseProcessor):
         data = infer_output[0]
         vals = data.flatten()
         top_k = vals.argsort()[-1:-2:-1]
-        for n in top_k:
-            object_class = self.get_gesture_categories(n)
         if len(top_k):
             object_class = self.get_gesture_categories(top_k[0])
             origin_img = Image.fromarray(origin_img)
             draw = ImageDraw.Draw(origin_img)
             font = ImageFont.load_default()
             draw.text((10, 50), object_class, font=font, fill=255)
-            return np.array(origin_img), object_class
+            return np.array(origin_img)
 
-        return np.array(origin_img), object_class
+        return np.array(origin_img)
     
     def predict(self, frame):
         cv2.imwrite(self._tmp_file, frame)
         self._acl_image = AclImage(self._tmp_file)
         resized_image = self.preprocess(self._acl_image)
-        result = self.model.execute([resized_image,])
-        result, action = self.postprocess(result, frame)
-        return result, action
+        infer_out = self.model.execute([resized_image,])
+        result = self.postprocess(infer_out, frame)
+        return result
 
     def get_gesture_categories(self, gesture_id):
         if gesture_id >= len(ModelProcessor.gesture_categories):
